60d59e9a42c0a5db0d426928942c553dde5e72f9
SkyDoge69 <luc2002penta@gmail.com>
2020-10-24 13:20:15 +0300

    Added user model, error and database file

---
+++ database.py
@@ -0,0 +1,25 @@
+import sqlite3 as sqlite
+
+DB_NAME = "example.db"
+
+conn = sqlite.connect(DB_NAME)
+
+conn.cursor().execute('''
+CREATE TABLE IF NOT EXISTS user
+    (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL
+    )
+''')
+conn.commit()
+
+
+class SQLite(object):
+
+    def __enter__(self):
+        self.conn = sqlite.connect(DB_NAME)
+        return self.conn.cursor()
+
+    def __exit__(self, type, value, traceback):
+        self.conn.commit()
