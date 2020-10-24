03ebf3307b9f7426071bc95aa98607e36909c93d
SkyDoge69 <luc2002penta@gmail.com>
2020-10-24 20:08:51 +0300

    Registered users are added in database

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from form import *
+from model.user import User
+import json
 
 app = Flask(__name__)
 app.secret_key = 'shushumushu'
@@ -9,8 +11,24 @@ app.secret_key = 'shushumushu'
 def index():
     register_form = RegistrationForm()
     if register_form.validate_on_submit():
+        username = register_form.username.data
+        password = register_form.password.data
+
+        # have to check if username already in use
+        user = User(username, password)
+        print(user.name)
+        print(user.password)
+        print(user.id)
+        user.save()
         return render_template("choice.html", form = register_form)
     return render_template("index.html", form = register_form)
+
+@app.route("/users", methods=["GET"])
+def list_users():
+    result = []
+    for user in User.all():
+        result.append(user.to_viewable())
+    return jsonify(result), 201
 
 if __name__ == "__main__":
     app.run(debug=True)
