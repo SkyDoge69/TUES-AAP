55684e25ee1ad9c06cc3fb45ff5388cf8378ca06
SkyDoge69 <luc2002penta@gmail.com>
2020-10-28 19:42:37 +0200

    Added login form and page

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, redirect, url_for
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
+
 from form import *
 from model.user import User
 import json
 
 app = Flask(__name__)
 app.secret_key = 'shushumushu'
+login_manager = LoginManager()
+login_manager.init_app(app)
 
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.find(user_id)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
@@ -21,8 +29,17 @@ def register():
         print(register_form.username.data)
         user = User(username, password, "Hasn't chosen")
         user.save()
-        return render_template("choice.html", form = register_form)
+        return redirect(url_for('login'))
     return render_template("registration.html", form = register_form)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    login_form = LoginForm()
+
+    if login_form.validate_on_submit():
+        return "Logged in, hopefully!"
+
+    return render_template("login.html", form = login_form)
 
 @app.route("/choice", methods=["GET", "POST"])
 def choice():
--- form.py
+++ form.py
@@ -3,6 +3,17 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, EqualTo, ValidationError
 from model.user import User
 
+
+def invalid_credentials(form, field):
+    username_entered = form.username.data
+    password_entered = field.data
+    
+    user_object = User.find_by_name(username_entered)
+    if user_object is None:
+        raise ValidationError("Wrong credentials!")
+    elif password_entered != user_object.password:
+        raise ValidationError("Wrong credentials!")
+
 class RegistrationForm(FlaskForm):
     """ Registration form """
     username = StringField('username_label', validators=[InputRequired(message="Username required"), 
@@ -23,3 +34,8 @@ class RegistrationForm(FlaskForm):
         does_exist = User.find_by_name(username.data)
         if does_exist != None:
             raise ValidationError("Someone has taken this username, be more original!")
+
+class LoginForm(FlaskForm):
+    username = StringField('username_label', validators=[InputRequired(message="Username required")])
+    password = PasswordField('password_label', validators=[InputRequired(message="Password required"), invalid_credentials])
+    submit_button = SubmitField('login')
